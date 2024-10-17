7970ba8ac2d5aa41c74926b61bc625f6eb4db85c
Francisca Silva <franciscafpsilva@ua.pt>
2024-10-17 09:06:28 +0100

    Start implementing the create alert page

---
+++ web/src/app/alerts/create/page.tsx
@@ -0,0 +1,23 @@
+import { AlertCollapse } from "@/components/AlertCollapse";
+
+export default function Create() {
+  return (
+    <div className="p-12">
+      <div className="justify-right">
+        <div className="">
+          <h1 className="text-4xl font-semibold font-alt gap-2 py-2  ">
+            Create an Alert
+          </h1>
+        </div>
+      </div>
+      <div className="py-12">
+        <h2 className="text-2xl font-semibold">
+          Alerts send:
+        </h2>
+      </div>
+
+
+
+    </div>
+  );
+}
