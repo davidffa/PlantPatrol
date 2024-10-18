e73a7478cc2b5de4824fdd34c70cb023604ebd5d
Francisca Silva <franciscafpsilva@ua.pt>
2024-10-18 22:03:49 +0100

    Add Create an alert page

--- web/src/app/alerts/create/page.tsx
+++ web/src/app/alerts/create/page.tsx
@@ -1,4 +1,5 @@
 import { AlertCollapse } from "@/components/AlertCollapse";
+import Link from "next/link";
 
 export default function Create() {
   return (
@@ -10,14 +11,50 @@ export default function Create() {
           </h1>
         </div>
       </div>
-      <div className="py-12">
-        <h2 className="text-2xl font-semibold">
-          Alerts send:
-        </h2>
+      <form>
+      <div className="flex justify-between">
+        <div className="py-8 w-4/6" >
+          <h2 className="text-2xl font-semibold mb-3">
+            Title:
+          </h2>
+          <input required type="text" placeholder="Type here" className="input input-md input-bordered w-full" />
+        </div>
+        <div className="py-8">
+          <h2 className="text-2xl font-semibold mb-3">
+            Send to:
+          </h2>
+          <div>
+            <select name="Send" id="send" className="menu menu-dropdown bg-base-100 rounded-box  z-[1] w-52 h-12  p-3 border  border-gray-300 ">
+              <option value="Everyone">Everyone</option>
+              <option value="Paulo">Paulo Miranda</option>
+              <option value="Joaquim">Joaquim Costa</option>
+              <option value="Rosa">Rosa Marques</option>
+            </select>
+          </div>
+        </div>
       </div>
-
-
-
+      <div>
+        <h2 className="text-2xl font-semibold mb-3">
+          Description:
+        </h2>
+        <textarea required id="description" className="textarea textarea-bordered textarea-xl min-w-full min-h-64" placeholder="Alert description"></textarea>
+        
+      </div>
+      <div className="flex justify-between mt-3">
+      <div>
+        <Link href="/alerts">
+          <button className=" py-2 px-6 gap-2 " >
+            <img src="/arrow-left.svg" alt="Back" />
+          </button>
+        </Link>
+      </div>
+      <div>
+        <button className="bg-green hover:bg-dark-green hover:duration-200 rounded-full py-2 px-6 justify-center items-center gap-2 " >
+          <p className="text-2xl font-semibold text-white">Send</p>
+        </button>
+      </div>
+      </div>
+      </form>
     </div>
   );
 }
