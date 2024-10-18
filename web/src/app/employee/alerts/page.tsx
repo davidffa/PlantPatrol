29de9cefa08d4deb0764f73a4767c7b4a2d13cb3
Francisca Silva <franciscafpsilva@ua.pt>
2024-10-18 23:03:06 +0100

    Add the employee alert page and update components

--- web/src/app/employee/alerts/page.tsx
+++ web/src/app/employee/alerts/page.tsx
@@ -1,52 +1,41 @@
-import { AlertCollapse } from "@/components/AlertCollapse";
+
+import { AlertCollapseReciver } from "@/components/AlertCollapseReciver";
 import Link from "next/link";
 
 export default function Alerts() {
   return (
     <div className="p-12">
-      <div className="flex justify-between">
-        <div className="">
-          <h1 className="text-4xl font-semibold font-alt gap-2 py-2  ">
+      
+        <div className="mb-12">
+          <h1 className="text-4xl font-semibold font-alt gap-2 py-2 mb-6 ">
             Alerts
           </h1>
+          <hr className="mb-4"/>
         </div>
 
-        <Link href="/alerts/create">
-          <button className="bg-green hover:bg-dark-green hover:duration-200 rounded-full flex py-2 px-6 justify-center items-center gap-2 " >
-            <img src="/plus.svg" alt="Adicionar" />
-            <p className="text-2xl font-semibold text-white">New</p>
-          </button>
-        </Link>
-
-      </div>
-      <div className="py-12">
-        <h2 className="text-2xl font-semibold">
-          Alerts send:
-        </h2>
-      </div>
-      
-      <AlertCollapse
+    <AlertCollapseReciver
+        title="SYSTEM: Check temp. sensor no. 2 at Greenhouse 2"
+        data="13/10/2024, 1:40PM"
+        description="We're running out of fertilizer, please order at least 100L until next week."
+      />
+      <AlertCollapseReciver
         title="Order more fertilizer"
         data="12/10/2024, 6:20PM"
-        sender="Everyone"
         description="We're running out of fertilizer, please order at least 100L until next week."
       />
-      <AlertCollapse
+      <AlertCollapseReciver
         title="Urgent meeting!"
         data="10/10/2024, 4:19PM"
-        sender="Everyone"
         description="We're running out of fertilizer, please order at least 100L until next week."
       />
-      <AlertCollapse
+      <AlertCollapseReciver
         title="Replace faulty sensour at greenhouse 3!"
         data="4/10/2024, 3:35PM"
-        sender="Everyone"
         description="We're running out of fertilizer, please order at least 100L until next week."
       />
-      <AlertCollapse
+      <AlertCollapseReciver
         title="Improve watering rules"
         data="3/10/2024, 10:12AM"
-        sender="Everyone"
         description="We're running out of fertilizer, please order at least 100L until next week."
       />
 
--- web/src/components/AlertCollapseReciver.tsx
+++ web/src/components/AlertCollapseReciver.tsx
@@ -3,11 +3,10 @@ import { ReactNode } from "react";
 type Props = {
   title: string;
   data: string;
-  sender: string;
   description: string;
 }
 
-export function AlertCollapse({ title, data,sender, description}: Props) {
+export function AlertCollapseReciver({ title, data, description}: Props) {
   return (
     <div tabIndex={0} className="collapse collapse-arrow border-base-300 bg-base-200 border  mb-4">
       <input type="checkbox" />
@@ -17,7 +16,7 @@ export function AlertCollapse({ title, data,sender, description}: Props) {
           </div>
         <div className="collapse-content">
           <hr className="mb-4"/>
-          <p>Sent to: {sender}</p>
+          <p>From: João Ramalho</p>
           <br/>
           <p>Description:</p>
           <p>{description}</p>
