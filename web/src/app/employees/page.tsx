3b2186126208d09a62a03215c12aaae670a59cb4
David Amorim <davidfilipeamorim@gmail.com>
2024-10-19 21:13:38 +0100

    Add new employee page

--- web/src/app/employees/page.tsx
+++ web/src/app/employees/page.tsx
@@ -1,6 +1,7 @@
 import { EmployeeCard } from "@/components/EmployeeCard"
 import { Navbar } from "@/components/Navbar"
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Employees() {
   return (
@@ -9,15 +10,17 @@ export default function Employees() {
 
       <div className="flex justify-between px-16 mt-10">
         <h1 className="font-alt font-bold text-4xl">Employee Management</h1>
-        <button className="bg-green py-4 px-8 rounded-full flex gap-4 items-center hover:bg-dark-green hover:duration-200">
-          <Image
-            src="/plus.svg"
-            alt="Add"
-            height={42}
-            width={42}
-          />
-          <span className="text-white font-medium text-2xl">New</span>
-        </button>
+        <Link href="/employees/new" className="rounded-full">
+          <button className="bg-green py-4 px-8 rounded-full flex gap-4 items-center hover:bg-dark-green hover:duration-200">
+            <Image
+              src="/plus.svg"
+              alt="Add"
+              height={42}
+              width={42}
+            />
+            <span className="text-white font-medium text-2xl">New</span>
+          </button>
+        </Link>
       </div>
 
       <div className="p-16 overflow-y-scroll flex flex-col gap-8">
