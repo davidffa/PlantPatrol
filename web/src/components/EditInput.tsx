c35ac4634d0ebcdec997de0e881f31c0fc86c953
Francisca Silva <franciscafpsilva@ua.pt>
2024-10-20 14:19:34 +0100

    Update and refactor some code

--- web/src/components/EditInput.tsx
+++ web/src/components/EditInput.tsx
@@ -2,51 +2,49 @@
 import { useState } from "react";
 
 interface Props {
-    min: number;
-    minvalue:number;
-    onChange: (minimum: number) => void;
+	min: number;
+	minvalue:number;
+	onChange: (minimum: number) => void;
 }
 
 const EditInput = ({min, minvalue, onChange}:Props) => {
-    const [isInputVisible, setIsInputVisible] = useState(false);
-    const [inputValue, setInputValue] = useState('');
+	const [isInputVisible, setIsInputVisible] = useState(false);
+	const [inputValue, setInputValue] = useState('');
+	const [inputValue2, setInputValue2] = useState(String(min));
 
-    const [inputValue2, setInputValue2] = useState(String(min));
+	const editInput = () => {
+		if (isInputVisible) {
+			setInputValue('');
+		}
+		setIsInputVisible((prev) => !prev);
+	};
 
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newValue =(e.target.value);
+		setInputValue2(newValue); 
+		min = Number(newValue);
+		onChange(min);
+	};
 
-    const editInput = () => {
-        if (isInputVisible) {
-            setInputValue(''); // Limpa o valor ao esconder o input
-          }
-          setIsInputVisible((prev) => !prev);
-    };
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newValue =(e.target.value);
-        setInputValue2(newValue); // Atualiza o estado local
-        min = Number(newValue);
-        onChange(min);
-      };
-
-    return (
-        <div>
-            {!isInputVisible && 
-            <div className="flex">
-                <div className="flex mt-12 text-right text-white justify-between px-8" >{minvalue}</div>
-                    <button className='mt-12' onClick={editInput} >
-                        <img src="/Vector.svg" alt="Editar"/>
-                    </button>
-            </div>
-            }
-        {isInputVisible && (
-            <div className="flex ">
-                <input type="text" className="h-6 border rounded w-11 bg-transparent text-white border-white mt-12 " value={inputValue2} onChange={handleInputChange}/>
-                <img className="mt-12 " src="/Vector.svg" alt="Editar" onClick={editInput} />
-            </div>
-        )}
-        </div>
-    );
+	return (
+		<div>
+			{isInputVisible ? (
+					<div className="flex ">
+						<input type="text" className="h-6 border rounded w-11 bg-transparent text-white border-white mt-12 " value={inputValue2} onChange={handleInputChange}/>
+						<img className="mt-12 " src="/Vector.svg" alt="Editar" onClick={editInput} />
+					</div>
+				) : 
+				(
+					<div className="flex">
+						<div className="flex mt-12 text-right text-white justify-between px-8" >{minvalue}</div>
+						<button className='mt-12' onClick={editInput} >
+							<img src="/Vector.svg" alt="Editar"/>
+						</button>
+					</div>
+				)
+			}
+		</div>
+	);
 };
-
 
 export default EditInput;
--- web/src/components/InventoryCard.tsx
+++ web/src/components/InventoryCard.tsx
@@ -1,88 +1,83 @@
 import React, {useState} from 'react'
 import EditInput from './EditInput';
+import Image from 'next/image'
 type Props = {
-    title: string,
-    image: string,
-    available: number,
-    minimum: number,
-    manager: boolean
+	title: string,
+	image: string,
+	available: number,
+	minimum: number,
+	manager?: boolean
 }
 
+export default function InventoryCard({ title, image, available, minimum, manager=false}: Props){
+	
+	const [min, setValue] = useState(minimum); 
+	const [ava, setValue2] = useState(available); 
+	const handleValueChange = (newValue: number) => {
+		setValue(newValue); 
+	};
+	const handleValueChange2 = (newValue: number) => {
+		setValue2(newValue); 
+	};
 
-export default function InventoryCard({ title, image, available, minimum, manager}: Props){
-    
-    const [min, setValue] = useState(minimum); 
-    const [ava, setValue2] = useState(available); 
-    const handleValueChange = (newValue: number) => {
-      setValue(newValue); 
-    };
-    const handleValueChange2 = (newValue: number) => {
-      setValue2(newValue); 
-    };
-
-    return (
-        <div className="card cursor-pointer shadow-md  hover:shadow-green hover:translate-y-[-4px]  transition-all ease-in-out image-full w-60" >
-            <figure>
-                <img
-                    src={image}
-                    alt={title}
-                />
-            </figure>
-            <div className="card-body ">
-                {manager &&
-                    <div className="w-full  flex flex-col ">
-                        <div className='h-4/6 w-full  text-left text-2xl text-white'>
-                            {title}
-                        </div>
-                        <div className=' h-1/6 w-full grid grid-cols-2 align-bottom'>
-                            <div className='flex w-full text-left text-white justify-between text-md mt-12'>
-                                <p>Available:</p>
-                            </div>
-                            <div className='flex  text-right text-white justify-between mt-12 px-6'>{available}</div>
-                            <div>
-                                
-                            </div> 
-                        </div>
-                        <div className='h-1/6 flex w-full align-bottom justify-between '>
-                            <div className='flex  text-left text-white justify-between mt-12 '>
-                                <p>Minimum:</p>
-                            </div>
-                            <div >
-                                <EditInput min={minimum} minvalue={min} onChange={handleValueChange}/>
-                            
-                            </div> 
-                        </div>               
-                    </div>
-                }
-                {!manager &&
-                    <div className="w-full  flex flex-col ">
-                        <div className='h-4/6 w-full  text-left text-2xl text-white'>
-                            {title}
-                        </div>
-                        <div className='h-1/6 flex w-full align-bottom justify-between '>
-                            <div className='flex  text-left text-white justify-between mt-12 '>
-                                <p>Available:</p>
-                            </div>
-                            <div>
-                                <EditInput min={available} minvalue={ava} onChange={handleValueChange2}/>
-                            </div> 
-                        </div>
-                        <div className=' h-1/6 w-full grid grid-cols-2 align-bottom'>
-                            <div className='flex w-full text-left text-white justify-between text-md mt-12'>
-                                <p>Minimum:</p>
-                            </div>
-                            <div className='flex  text-right text-white justify-between mt-12 px-6'>{minimum}</div>
-                            <div>
-                                
-                            </div> 
-                        </div>
-                    </div>
-
-                }
-                    
-                    
-                
-            </div>
-        </div>
-    )
+	return (
+		<div className="card cursor-pointer shadow-md  hover:shadow-green hover:translate-y-[-4px]  transition-all ease-in-out image-full w-60" >
+			<figure>
+				<Image
+					src={image}
+					alt={title}
+					height={300}
+					width={350}
+				/>
+			</figure>
+			<div className="card-body ">
+				{manager ? 
+					(
+					<div className="w-full  flex flex-col ">
+						<div className='h-4/6 w-full  text-left text-2xl text-white'>
+							{title}
+						</div>
+						<div className=' h-1/6 w-full grid grid-cols-2 align-bottom'>
+							<div className='flex w-full text-left text-white justify-between text-md mt-12'>
+								<p>Available:</p>
+							</div>
+							<div className='flex  text-right text-white justify-between mt-12 px-6'>{available}</div>
+							<div></div> 
+						</div>
+						<div className='h-1/6 flex w-full align-bottom justify-between '>
+							<div className='flex  text-left text-white justify-between mt-12 '>
+								<p>Minimum:</p>
+							</div>
+							<div >
+								<EditInput min={minimum} minvalue={min} onChange={handleValueChange}/>
+							</div> 
+						</div>               
+					</div>
+					) :
+					(
+					<div className="w-full  flex flex-col ">
+						<div className='h-4/6 w-full  text-left text-2xl text-white'>
+							{title}
+						</div>
+						<div className='h-1/6 flex w-full align-bottom justify-between '>
+							<div className='flex  text-left text-white justify-between mt-12 '>
+								<p>Available:</p>
+							</div>
+							<div>
+								<EditInput min={available} minvalue={ava} onChange={handleValueChange2}/>
+							</div> 
+						</div>
+						<div className='h-1/6 w-full grid grid-cols-2 align-bottom'>
+							<div className='flex w-full text-left text-white justify-between text-md mt-12'>
+								<p>Minimum:</p>
+							</div>
+							<div className='flex  text-right text-white justify-between mt-12 px-6'>{minimum}</div>
+							<div></div> 
+						</div>
+					</div>
+					)
+				}
+			</div>
+		</div>
+	)
 }
