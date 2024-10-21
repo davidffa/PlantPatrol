4db7bd4182381b785595ff87ff4b33510c2dc50c
David Amorim <davidfilipeamorim@gmail.com>
2024-10-21 15:36:51 +0100

    Refactor create alert page scheme

--- web/src/app/alerts/create/page.tsx
+++ web/src/app/alerts/create/page.tsx
@@ -1,32 +1,39 @@
+"use client"
+
 import { Navbar } from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
+import { FormEvent } from "react";
 
 export default function Create() {
+  function handleCreateAlert(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    alert("Form sent!")
+  }
+
   return (
-    <>
+    <div className="h-screen flex flex-col">
       <Navbar />
 
-      <div className="px-12 py-6">
-        <div className="justify-right">
-          <h1 className="text-4xl font-semibold font-alt gap-2 py-2  ">
-            Create an Alert
-          </h1>
-        </div>
-        <form>
-          <div className="flex justify-between">
-            <div className="py-8 w-4/6" >
-              <h2 className="text-2xl font-semibold mb-3">
-                Title:
-              </h2>
-              <input required type="text" placeholder="Type here" className="input input-md input-bordered w-full" />
-            </div>
-            <div className="py-8">
-              <h2 className="text-2xl font-semibold mb-3">
-                Send to:
-              </h2>
-              <div>
-                <select name="Send" id="send" className="menu menu-dropdown bg-base-100 rounded-box  z-[1] w-52 h-12  p-3 border  border-gray-300 ">
+      <div className="px-12 mt-6 h-full flex flex-col">
+        <h1 className="text-4xl font-semibold font-alt gap-2 py-2">
+          Create an Alert
+        </h1>
+        <form className="flex flex-col justify-around h-full" onSubmit={handleCreateAlert}>
+          <div>
+            <div className="flex justify-between">
+              <div className="py-8 w-4/6" >
+                <h2 className="text-2xl font-semibold mb-3">
+                  Title:
+                </h2>
+                <input required type="text" placeholder="Type here" className="input input-md input-bordered w-full" />
+              </div>
+              <div className="py-8">
+                <h2 className="text-2xl font-semibold mb-3">
+                  Send to:
+                </h2>
+                <select id="send" className="menu menu-dropdown bg-base-100 rounded-box z-[1] w-52 h-12 p-3 border border-gray-300 ">
                   <option value="Everyone">Everyone</option>
                   <option value="Paulo">Paulo Miranda</option>
                   <option value="Joaquim">Joaquim Costa</option>
@@ -34,26 +41,24 @@ export default function Create() {
                 </select>
               </div>
             </div>
-          </div>
-          <div>
-            <h2 className="text-2xl font-semibold mb-3">
-              Description:
-            </h2>
-            <textarea required id="description" className="textarea textarea-bordered textarea-xl min-w-full min-h-64" placeholder="Alert description"></textarea>
+            <div>
+              <h2 className="text-2xl font-semibold mb-3">
+                Description:
+              </h2>
+              <textarea required id="description" className="textarea textarea-bordered textarea-xl min-w-full min-h-64 resize-none" placeholder="Alert description" />
 
+            </div>
           </div>
           <div className="flex justify-between mt-6">
             <Link href="/alerts" className="rounded">
               <Image src="/arrow-left.svg" alt="Back" width={42} height={42} />
             </Link>
-            <div>
-              <button className="bg-green hover:bg-dark-green hover:duration-200 rounded-full py-2 px-6 justify-center items-center gap-2 " >
-                <p className="text-2xl font-semibold text-white">Send</p>
-              </button>
-            </div>
+            <button type="submit" className="bg-green hover:bg-dark-green hover:duration-200 rounded-full py-2 px-6 justify-center items-center gap-2">
+              <p className="text-2xl font-semibold text-white">Send</p>
+            </button>
           </div>
         </form>
       </div>
-    </>
+    </div>
   );
 }
