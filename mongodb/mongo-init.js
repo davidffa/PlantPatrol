c7769dffc3c2ef27609e36d3744c3d236e2e40de
David Amorim <davidfilipeamorim@gmail.com>
2024-11-04 11:55:41 +0000

    Setup docker compose

---
+++ mongodb/mongo-init.js
@@ -0,0 +1,15 @@
+db.employees.insertOne({
+  "_id": {
+    "$oid": "671faeecb36fa4367f435fa1"
+  },
+  "username": "admin",
+  "password": "$2a$10$WatqARWbDf4N.zKgQS0.m.InvqMZYR1Qb/ae4hc0C0QzfAPuCJPje",
+  "isManager": true,
+  "firstName": "Paulo",
+  "lastName": "Miranda",
+  "phoneNumber": "123456789",
+  "_class": "pt.ua.deti.ies.plantpatrol.backend.entity.Employee",
+  "birthDate": {
+    "$date": "2004-01-06T00:00:00.000Z"
+  }
+})
