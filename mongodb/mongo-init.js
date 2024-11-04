d5a1df938043ad0cc0721279a6393bad3cb23579
FranciscaPedro <120456821+FranciscaPedro@users.noreply.github.com>
2024-11-04 17:17:18 +0000

    Update mongo-init.js

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -2,8 +2,8 @@ db.employees.insertOne({
   "username": "admin",
   "password": "$2a$10$WatqARWbDf4N.zKgQS0.m.InvqMZYR1Qb/ae4hc0C0QzfAPuCJPje",
   "isManager": true,
-  "firstName": "Paulo",
-  "lastName": "Miranda",
+  "firstName": "João",
+  "lastName": "Ramalho",
   "phoneNumber": "123456789",
   "_class": "pt.ua.deti.ies.plantpatrol.backend.entity.Employee",
   "birthDate": new Date("2004-01-06T00:00:00.000Z")
