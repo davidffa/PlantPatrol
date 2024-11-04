6954545a83dda33a142f92ff6ecdca7c2bf79c05
David Amorim <davidfilipeamorim@gmail.com>
2024-11-04 13:03:36 +0000

    Fix mongodb init script

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -1,7 +1,4 @@
 db.employees.insertOne({
-  "_id": {
-    "$oid": "671faeecb36fa4367f435fa1"
-  },
   "username": "admin",
   "password": "$2a$10$WatqARWbDf4N.zKgQS0.m.InvqMZYR1Qb/ae4hc0C0QzfAPuCJPje",
   "isManager": true,
@@ -9,7 +6,5 @@ db.employees.insertOne({
   "lastName": "Miranda",
   "phoneNumber": "123456789",
   "_class": "pt.ua.deti.ies.plantpatrol.backend.entity.Employee",
-  "birthDate": {
-    "$date": "2004-01-06T00:00:00.000Z"
-  }
+  "birthDate": new Date("2004-01-06T00:00:00.000Z")
 })
