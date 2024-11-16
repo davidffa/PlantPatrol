8fdd951d2845ee5ce5e8f90f44374386e2495356
David Amorim <davidfilipeamorim@gmail.com>
2024-11-16 14:59:46 +0000

    Implement reminders endpoints

---
+++ backend/src/main/java/pt/ua/deti/ies/plantpatrol/backend/controller/ReminderController.java
@@ -0,0 +1,48 @@
+package pt.ua.deti.ies.plantpatrol.backend.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pt.ua.deti.ies.plantpatrol.backend.dto.CreateReminderDTO;
+import pt.ua.deti.ies.plantpatrol.backend.response.ErrorResponse;
+import pt.ua.deti.ies.plantpatrol.backend.service.ReminderService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ReminderController {
+    private final ReminderService reminderService;
+
+    public ReminderController(ReminderService reminderService) {
+        this.reminderService = reminderService;
+    }
+
+    @PostMapping("/reminders")
+    public ResponseEntity<?> addReminder(@RequestBody CreateReminderDTO dto) {
+        try {
+            reminderService.addPlant(dto.getClientId(), dto.getPlantId());
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/reminders/{plantId}")
+    public ResponseEntity<?> removeReminder(HttpServletRequest request, @PathVariable String plantId) {
+        String clientId = request.getHeader("clientId");
+
+        if (clientId == null) {
+            return new ResponseEntity<>(new ErrorResponse("Missing client id"), HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            reminderService.removePlant(UUID.fromString(clientId), plantId);
+
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
