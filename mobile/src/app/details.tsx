d5b27e57c087d4c801620dcf39cf0c5aa891f7b5
David Amorim <davidfilipeamorim@gmail.com>
2024-11-16 15:55:26 +0000

    Fix details screen scrolling

--- mobile/src/app/details.tsx
+++ mobile/src/app/details.tsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { plants as staticPlants } from "@/utils/plants";
 import { SafeAreaView } from "react-native-safe-area-context";
-
 
 export default function Details() {
   const router = useRouter();
@@ -29,36 +28,38 @@ export default function Details() {
   }
 
   return (
-    <SafeAreaView className="flex-1 ">
+    <SafeAreaView className="flex-1">
       <View className="m-4 justify-between flex-row">
         <Feather name="arrow-left" size={45} onPress={router.back} />
         <TouchableOpacity className={` ${alert && "bg-red-600 rounded-full p-1.5"}`} onPress={toggleAlert} activeOpacity={0.7}>
           <Feather name="bell" color={alert ? "white" : "black"} size={alert ? 36 : 45} />
         </TouchableOpacity>
       </View>
-      <View className="items-center justify-center h-1/3">
-        <Image source={getPlant(id).imageUrl} className="h-full" />
-      </View>
-      <View className=" bg-gray-200 rounded-3xl h-1/2 mx-4 mt-12 ">
-        <View className="flex-row justify-between mt-6">
-          <Text className="text-lg ml-6 py-2 font-bold">___ Best choice</Text>
-          {alert ?
-            <View className=" bg-red-500 rounded-l-3xl items-center justify-center py-2 px-3 ">
-              <Text className="text-xl color-white gap-2">Not Available</Text>
-            </View>
-            :
-            <View className=" bg-green rounded-l-3xl items-center justify-center py-2 px-3 ">
-              <Text className="text-xl color-white gap-2">Available</Text>
-            </View>
-          }
+      <ScrollView>
+        <View className="items-center justify-center h-1/2">
+          <Image source={getPlant(id).imageUrl} className="h-full" />
         </View>
-        <Text className="text-3xl ml-6 py-4 font-bold">{getPlant(id).name}</Text>
-        <Text className="text-lg ml-6 py-2 font-italic">Asphodelaceae</Text>
-        <Text className="text-lg ml-6 py-2 font-bold">About</Text>
-        <Text className="text-lg ml-6">{getPlant(id).about}</Text>
-        <Text className="text-lg ml-6 py-2 font-bold">Curiosities</Text>
-        <Text className="text-lg ml-6">{getPlant(id).curiosities}</Text>
-      </View>
+        <View className="bg-gray-200 rounded-3xl mx-4 mt-12 py-8">
+          <View className="flex-row justify-between">
+            <Text className="text-lg ml-6 py-2 font-bold">___ Best choice</Text>
+            {alert ?
+              <View className=" bg-red-500 rounded-l-3xl items-center justify-center py-2 px-3 ">
+                <Text className="text-xl color-white gap-2">Not Available</Text>
+              </View>
+              :
+              <View className=" bg-green rounded-l-3xl items-center justify-center py-2 px-3 ">
+                <Text className="text-xl color-white gap-2">Available</Text>
+              </View>
+            }
+          </View>
+          <Text className="text-3xl ml-6 py-4 font-bold">{getPlant(id).name}</Text>
+          <Text className="text-lg ml-6 py-2 font-italic">Asphodelaceae</Text>
+          <Text className="text-lg ml-6 py-2 font-bold">About</Text>
+          <Text className="text-lg ml-6">{getPlant(id).about}</Text>
+          <Text className="text-lg ml-6 py-2 font-bold">Curiosities</Text>
+          <Text className="text-lg ml-6">{getPlant(id).curiosities}</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
