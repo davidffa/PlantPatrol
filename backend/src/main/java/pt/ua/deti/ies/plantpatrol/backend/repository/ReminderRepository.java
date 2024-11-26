d7a58ad3bb52cccd075b1fccf2392ad7e0b308bb
David Amorim <davidfilipeamorim@gmail.com>
2024-11-26 16:45:44 +0000

    Implement push notifications sending in backend

--- backend/src/main/java/pt/ua/deti/ies/plantpatrol/backend/repository/ReminderRepository.java
+++ backend/src/main/java/pt/ua/deti/ies/plantpatrol/backend/repository/ReminderRepository.java
@@ -1,10 +1,15 @@
 package pt.ua.deti.ies.plantpatrol.backend.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import pt.ua.deti.ies.plantpatrol.backend.entity.Reminder;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReminderRepository extends MongoRepository<Reminder, String> {
     Optional<Reminder> findByClientId(String clientId);
+
+    @Query("{ plants: { $eq: ?0 } }")
+    List<Reminder> findRemindersForPlantId(String plantId);
 }
