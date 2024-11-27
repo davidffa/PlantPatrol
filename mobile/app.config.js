459ff8a90fe4a4a1198a14874da7ac382fbaf4d2
David Amorim <davidfilipeamorim@gmail.com>
2024-11-27 11:30:35 +0000

    Add google services json to gitignore

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "expo": {
     "name": "PlantPatrol",
     "slug": "plant-patrol-mobile",
@@ -22,7 +22,7 @@
         "backgroundColor": "#ffffff"
       },
       "package": "pt.ua.deti.ies.plantpatrol.mobile",
-      "googleServicesFile": "google-services.json"
+      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON
     },
     "web": {
       "favicon": "./assets/favicon.png"
