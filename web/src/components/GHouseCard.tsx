145adb9bd31411d7a9a1c3357db3f11f0918f6c4
Francisca Silva <franciscafpsilva@ua.pt>
2024-12-02 21:07:02 +0000

    Sensors data in the cards

--- web/src/components/GHouseCard.tsx
+++ web/src/components/GHouseCard.tsx
@@ -1,22 +1,60 @@
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from 'react';
 import Image from 'next/image'
 import SolarPower from '@mui/icons-material/SolarPower'
 import OpacityIcon from '@mui/icons-material/Opacity';
 import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
 import Co2Icon from '@mui/icons-material/Co2';
+import api from '@/services/api';
 
 type Props = {
   greenhouse: string,
   id: number | string,
-  image: string,
-  humidity: number,
-  temperature: number,
-  aiq: number,
-  uv: number,
+  image: string
 }
 
-export default function GHouseCards({ id, greenhouse, image, humidity, temperature, aiq, uv }: Props) {
+type SensorReading = {
+  controllerId: string;
+  readingType: ReadingType;
+  temperature: number;
+  humidity: number;
+  aiq: number;
+  uv: number;
+  timestamp: string;
+};
+
+type ReadingType = "INSTANT" | "HOURLY" | "DAILY" | "WEEKLY" | "MONTHLY";
+
+export default function GHouseCards({ id, greenhouse, image }: Props) {
   const router = useRouter();
+  const [humidity, setHumidity] = useState<number>(0);
+  const [uv, setUv] = useState<number>(0);
+  const [temperature, setTemperature] = useState<number>(0);
+  const [aiq, setAiq] = useState<number>(0);
+  const [lista, setLista] = useState<number[]>([]);
+
+  useEffect(() => {
+    async function getAVG() {
+      const { data } = await api.get<SensorReading[]>(`/greenhouse/${id}/sensors-data`, { params: { "type": "INSTANT" } });
+
+      setLista(data.map(r => r.humidity));
+      lista.forEach(element => { setHumidity(humidity + element) });
+      setHumidity(humidity / lista.length);
+
+      setLista(data.map(r => r.uv));
+      lista.forEach(element => { setUv(uv + element) });
+      setUv(uv / lista.length);
+
+      setLista(data.map(r => r.temperature));
+      lista.forEach(element => { setTemperature(temperature + element) });
+      setTemperature(temperature / lista.length);
+
+      setLista(data.map(r => r.aiq));
+      lista.forEach(element => { setAiq(aiq + element) });
+      setAiq(aiq / lista.length);
+    }
+    getAVG();
+  }, []);
   return (
     <>
       <div className="card cursor-pointer shadow-md  hover:shadow-green hover:translate-y-[-4px]  transition-all ease-in-out image-full w-full " onClick={() => router.push(`/greenhouses/${id}`)}>
